11517c325ad7016ce0e357a937322583decae84b
Michał Krzemień <michalkrz98@gmail.com>
2021-10-27 20:31:12 +0200

    Change in constructor and way of generating points list

--- Jacobian.py
+++ Jacobian.py
@@ -4,17 +4,20 @@ from Parameters import Parameters
 class Element4_2D:
     dNdE = []
     dNdN = []
-    a = Parameters(2)
-    points = [[a.points[0], a.points[0]],
-              [a.points[1], a.points[0]],
-              [a.points[1], a.points[1]],
-              [a.points[0], a.points[1]]]
+    points = []
 
     def __init__(self, npc):
-        self.dNdE = [[0 for _ in range(4)] for _ in range(npc)]
-        self.dNdN = [[0 for _ in range(4)] for _ in range(npc)]
-        for i in range(4):
-            self.dNdE[i][0] = -0.25 * (1-self.points[i][1])
+        self.dNdE = [[0 for _ in range(4)] for _ in range(pow(npc, 2))]
+        self.dNdN = [[0 for _ in range(4)] for _ in range(pow(npc, 2))]
+        self.points = [[0 for _ in range(2)] for _ in range(pow(npc, 2))]
+        param = Parameters(npc)
+        for i in range(npc):
+            for j in range(npc):
+                self.points[i * npc + j][0] = param.points[j]
+                self.points[i * npc +j][1] = param.points[i]
+
+        for i in range(pow(npc, 2)):
+            self.dNdE[i][0] = -0.25 * (1 - self.points[i][1])
             self.dNdE[i][1] = 0.25 * (1 - self.points[i][1])
             self.dNdE[i][2] = 0.25 * (1 + self.points[i][1])
             self.dNdE[i][3] = -0.25 * (1 + self.points[i][1])
@@ -26,10 +29,10 @@ class Element4_2D:
 
 
 if __name__ == "__main__":
-    a = Element4_2D(4)
-    for k in range(4):
+    a = Element4_2D(3)
+    for k in range(9):
         print(a.dNdE[k])
     print()
-    for k in range(4):
+    for k in range(9):
         print(a.dNdN[k])
 
